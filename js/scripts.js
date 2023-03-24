ad43d69256355f65d4abb6714463dce0aebbac42
Joseph Wilfong <josephwilfong91@gmail.com>
2023-03-24 08:58:03 -0700

    Add submit button, questions for user to interact with

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  const form = document.getElementById("language");
+  form.addEventListener("submit", display);
+
+    function display(event){
+      
+    }
+});
